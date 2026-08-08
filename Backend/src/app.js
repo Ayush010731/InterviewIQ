c2f3996e249a2e6f98f3916631dafe1cc4f77a8e
Ayush010731 <ayushojha072004@gmail.com>
2026-08-08 13:54:38 +0530

    Fix production API URL

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -24,7 +24,7 @@ app.use(cors({
     credentials: true
 }))
 
-app.options('*', cors());
+
 
 const authRouter = require("./routes/auth.routes")
 const interviewRouter = require("./routes/interview.routes")
